3bf049f1f7d6d0af5f05ab811eda080c549f7d8c
DESKTOP-I1NR1CL\A-8 <DESKTOP-I1NR1CL\A-8>
2022-08-10 13:24:05 +0800

    Fix bug variable score

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -64,9 +64,8 @@ public class GameManager : MonoBehaviour {
 	}
 
 	void OnPlayerScored(){
-	
-		_score++;
-		scoreText.text = _score;
+		score++;
+		scoreText.text = score.ToString();
 	}
 
 	void SetPageState(PageState state){
